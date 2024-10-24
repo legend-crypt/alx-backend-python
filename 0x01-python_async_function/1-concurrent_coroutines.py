814a87d7ee06f2f8502e3daf92d7db1720802743
lord <konadulordkweku@gmail.com>
2024-10-24 15:13:20 +0000

    Refactor code to import wait_random function in 1-concurrent_coroutines.py and 2-measure_runtime.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """import asyncio"""
-wait_random = __import__("0-basic_async_syntax").wait_random
 import asyncio
 from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,9 +2,10 @@
 
 """Measure the runtime"""
 
-wait_n = __import__("1-concurrent_coroutines").wait_n
 import time
 import asyncio
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
